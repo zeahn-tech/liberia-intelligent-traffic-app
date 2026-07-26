755da9c782aa0fa3aa4b5d921a0daebdbb58e3f9
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-26 23:54:42 +0000

    Before: Provide the supabase-migration-v4.sql to copy and paste

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j1c6hrprgjo"
+    "revision": "0.95afq2h3seo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
