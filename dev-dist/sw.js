7cfe5f659f3a9562a5c651b99fae3bb16414c621
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-26 23:19:06 +0000

    Before: yes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i13n502n97"
+    "revision": "0.da79s8p82h"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
