7060f15cf017ae3756c0343d128314e6cf67aa51
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-26 23:47:42 +0000

    Before: 12. MEDIA SECURITY
    Securely manage:
    
    Images
    Videos
    Audio
    Documents
    
    Use Supabase Storage with appro...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2fm6ar2cbn"
+    "revision": "0.j1c6hrprgjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
