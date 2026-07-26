1ef5013b82b733b68468a127e8039a2a7d107708
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-26 23:16:58 +0000

    Before: 7. AI ANALYSIS PIPELINE
    Create a modular AI architecture.
    
    Example workflow:
    
    Evidence Upload
         ...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k962uj15fc8"
+    "revision": "0.i13n502n97"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
