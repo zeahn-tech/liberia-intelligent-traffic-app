f216ac950b6be7ed2c4e49e8cfa1413e5e0e3170
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 17:57:40 +0000

    Before: errors once more: Error: Failed to run sql query: ERROR: 42703: column "user_id" does not exist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1l6ckpgitko"
+    "revision": "0.mhb6o20fl7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
