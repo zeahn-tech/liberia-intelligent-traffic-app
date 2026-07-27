3cc8bd2a5ef200403b2313a21ecd67a24e505ef5
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 01:36:41 +0000

    Before: I TRIED CREATING AN ACCOUNT. THIS IS THE ERROR I ENCOUNTERED. PLEASE FIX THE ISSUE: Supabase is not ...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s6njrskau5g"
+    "revision": "0.ocp182vr1qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
