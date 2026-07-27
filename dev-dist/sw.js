0e33f71e5162abed5363c5214712312f58d8e8a9
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 12:18:07 +0000

    Before: still showing errors

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3dag3i28qe8"
+    "revision": "0.ki3o0lum9v8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
