f69e70b01fdbdc9cabce3d3e6b14e19c29fdd76d
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 12:42:50 +0000

    Before: See developer console errors:  WebSocket connection to 'wss://5173-85ccdb1f-3901-4346-b1e7-4333689d2...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.748422b1kho"
+    "revision": "0.4o2aued509g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
