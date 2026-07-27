d4e2321a85606171ad630906002b8d4bc3a50155
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 16:28:41 +0000

    Before: 26. SECURITY ARCHITECTURE
    Implement security as a core architectural requirement.
    
    Use:
    
    Supabase R...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h76gcv0dh7"
+    "revision": "0.9uhe11nrj9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
