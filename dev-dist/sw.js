9271a6d26de2f7a46ebbf6af9d662c5347ffca68
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 02:01:34 +0000

    Before: So there is no way to  leave the confirmation toggle on and fix the issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.29nk9318q7o"
+    "revision": "0.v38qu0th52"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
