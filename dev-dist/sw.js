c99ffb689d2969e7416da2b499a09b9d881c53b9
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 16:08:02 +0000

    Before: 25. AUDIT LOGGING
    Create comprehensive audit logging.
    
    Record security-sensitive actions including:...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o4t6rha153g"
+    "revision": "0.u8blrrhf3t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
