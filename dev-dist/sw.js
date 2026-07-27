958e9a9b3a76d0af19eb3048e367a5264a298608
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 01:11:58 +0000

    Before: Still returning errors: Failed to run sql query: ERROR:  42883: function citizen_role() does not exi...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vqon2qgihno"
+    "revision": "0.2gs7h1laa7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
