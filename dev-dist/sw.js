719c55035ad0f690ac9cb18cb582e3c53470260e
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 01:41:47 +0000

    Before: Still telling me sign-in failed when I click the create account button

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ocp182vr1qo"
+    "revision": "0.24qkeb17dk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
