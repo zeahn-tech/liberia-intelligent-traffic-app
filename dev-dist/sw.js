35a8cfa7ddaf908d621469c71e8c3526599e1d96
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 00:45:50 +0000

    Before: 15. AI COMMAND DASHBOARD
    Create a premium command dashboard showing:
    
    Total violations today
    Violat...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4u42qlinktg"
+    "revision": "0.3r7dqgrg10g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
