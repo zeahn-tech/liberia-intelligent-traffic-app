a90a24136dab025b98b5bb6a2db1d9f61fce56be
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 10:48:46 +0000

    Before: 20. POLICE OFFICER PORTAL
    Create a dedicated officer workspace.
    
    Officers should be able to:
    
    View ...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7i6qa9ifgvo"
+    "revision": "0.h9km90af0j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
