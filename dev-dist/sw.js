c4f46e1d0f2c11d03d960739c1eae552751135ef
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 11:26:45 +0000

    Before: It returns fail errors: Failed to run sql query: ERROR:  42601: syntax error at end of input LINE 0:...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.akhq1cc6m18"
+    "revision": "0.gsmjhmov1dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
