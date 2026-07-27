a8852d8fd17d021107f40e2d407ce04936312577
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 00:36:27 +0000

    Before: 14. LIBERIA GEOGRAPHIC SUPPORT
    The initial deployment must support all 15 counties of Liberia.
    
    Inc...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k29m7hanlm"
+    "revision": "0.fni2hst31ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
