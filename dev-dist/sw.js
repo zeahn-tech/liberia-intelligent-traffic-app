86c1c9456d4c28088a909db23e534e152d8fa99a
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 01:09:28 +0000

    Before: It returns errors: Error: Failed to run sql query: ERROR: 42804: default for column "role" cannot be...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dq492if2lko"
+    "revision": "0.vqon2qgihno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
