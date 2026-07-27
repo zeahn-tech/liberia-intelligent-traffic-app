abd82c82eda9c85a31d35cad291dcba4c428313e
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-27 18:06:43 +0000

    Before: 30. API ARCHITECTURE
    Create a modular API architecture.
    
    Separate:
    
    Authentication
    Users
    Roles
    Inci...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nk20nsnjqg"
+    "revision": "0.9nalp6k0e7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
