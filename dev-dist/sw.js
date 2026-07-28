41458449109f644001eaac71970ceb0aac48f0da
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-28 11:44:58 +0000

    Before: 35. NAVIGATION
    Create a professional application shell.
    
    Suggested navigation:
    
    Dashboard
    Live Oper...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.clkedbipq58"
+    "revision": "0.fm526oomt8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
