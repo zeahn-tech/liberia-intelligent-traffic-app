d9b4585a853129babd5a7ded6d8eddf54d85770c
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-28 13:00:25 +0000

    Before: I am on the Supabase free plan. How do I set it up?

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2eqb3budqdo"
+    "revision": "0.358d45b2gv8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
