053d0433c9b4c520c4cf46dbdc62a60abd714a2a
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-30 00:47:31 +0000

    Before: supabase-seed-data.sql is not available in the location you think it is located, including the other...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.941ol23g28"
+    "revision": "0.brsas60g6e8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
