2b47687553bb85a72575ebe6b9ffaefa56289d08
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-30 04:02:41 +0000

    Before: Please check across the app pages and screens if there are any 404 screens, long-time screen loading...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n72tikfpubg"
+    "revision": "0.g4dtfmvnmho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
