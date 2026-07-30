9e23266922bb15695b3ef5327c9a761fb13eb807
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-30 01:09:15 +0000

    Before: Please place all  part 1  to 4 in the public folder so that i can access them quickly

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cmh0t6rmmpg"
+    "revision": "0.3bss250re0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
