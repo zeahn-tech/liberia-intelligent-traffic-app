efdd105c7a47c8a895a6a21edd4f7c4e13ae1fe2
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-30 00:13:08 +0000

    Freebuff: 50. SEED DATA

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qts7epn1rqo"
+    "revision": "0.941ol23g28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
