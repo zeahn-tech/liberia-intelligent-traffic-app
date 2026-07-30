3cf77baf535b6fc093d55ea4a9e60a260220948a
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-07-30 13:45:27 +0000

    Before: Do not modify code.
    Review everything.
    
    Confirm:
    
    No crashes
    
    No runtime errors
    
    No build errors
    
    N...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6oaqlinei2g"
+    "revision": "0.ibu21k6jb48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
