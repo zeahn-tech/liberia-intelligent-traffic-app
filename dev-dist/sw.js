b4ed58a930d13fd59e4a2bdfc0ddbe361b63656e
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-08-28 20:34:55 +0000

    Before: Error: Failed to run sql query: ERROR: 42P01: relation "public.incident_status_history" does not exi...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.smerc99cvg8"
+    "revision": "0.pa2n6t14n9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
