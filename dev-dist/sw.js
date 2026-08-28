0b445fcbdacc610cc6d6d019f0ea3500d0cb4d6d
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-08-28 20:59:05 +0000

    Before: Demo data are still  appearing on the Traffic incident report screen

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ii3951hsno8"
+    "revision": "0.20q5pgr76kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
