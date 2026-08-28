dccf57d1f19d4ae15a1a93b958ff16710f0ba94d
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-08-28 19:58:58 +0000

    Before: Failed to run sql query: ERROR:  42703: column "owner_name" does not exist LINE 41:   AND owner_name...

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5ifok4a99e8"
+    "revision": "0.pab010oqg9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
