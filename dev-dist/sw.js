8df7a12fee827b925b4d076612ccc239cef8bd97
Emmanuel Y. Zeahn <77918593+zeahn-tech@users.noreply.github.com>
2026-08-28 19:56:32 +0000

    Before: Failed to run sql query: ERROR:  42703: column "user_id" does not exist LINE 39: DELETE FROM public....

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e7iklhueuc8"
+    "revision": "0.9agpm4aruoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
